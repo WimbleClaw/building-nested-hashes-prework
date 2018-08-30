67332bbedb3355fddc4b644693ee867ab7dbb2f6
Aisha D'Souza <aishad.souza@yahoo.com>
2018-08-30 11:25:43 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,9 @@ def fourth_challenge
           :attitude => "confused"
         }
         {
-          :name => 
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
         }
         }
       ]
